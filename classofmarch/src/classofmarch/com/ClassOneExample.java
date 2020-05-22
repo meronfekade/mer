56db885dc18431e9d443bada9b3851a1a55a2cdc
meronfekade <meronfekade8@gmail.com>
2020-05-22 10:52:39 -0400

    reviewed

---
+++ classofmarch/src/classofmarch/com/ClassOneExample.java
@@ -0,0 +1,21 @@
+package classofmarch.com;
+
+public class ClassOneExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name="james";
+		int salary=1200;
+		double bonus=230;
+		double total_salary=salary+bonus;
+		
+		System.out.println("the name of the employee is :" +name);
+		System.out.println("the salary of the employee is :" +salary);
+		System.out.println("the bonus of the employee is :" +bonus);
+		System.out.println("the total_salary of the employee is :"+total_salary);
+
+
+	}
+
+}
